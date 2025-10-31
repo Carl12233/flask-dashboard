1454d4753f4c5630ecc0ce099274c24103765a4a
Hanns <poleopliz@gmail.com>
2025-10-31 03:25:14 +0100

    enable debug mode

--- admin_app.py
+++ admin_app.py
@@ -20,5 +20,4 @@ def users():
     return jsonify(all_users)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
